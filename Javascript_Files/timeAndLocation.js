47e34f131ca3d54605a8684a191484171a2861ea
Josh McIntosh <josh.m96@hotmail.com>
2020-08-23 17:04:26 +1000

    uncommenented unnecsary code

--- Javascript_Files/timeAndLocation.js
+++ Javascript_Files/timeAndLocation.js
@@ -27,7 +27,6 @@
         for (var i = 0; i < 2; i++){
             basicTime = basicTime + timeNow[i];
         } 
-debugger;
         currentTime.twentyFourHour = ridNumbersAfterDecimal(timeNow);
         currentTime.twelveHour = moment(currentTime.twentyFourHour, ["HH.mm"]).format("hh:mm A");
         currentTime.twelveHour = ridFirstZero(currentTime.twelveHour);
